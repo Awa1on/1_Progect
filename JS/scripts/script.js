ad38c38970217f71445832a6ceae38dbe0fe99ad
Awa1on <strikunm@mail.ru>
2020-07-09 20:35:42 +0500

    js begin

--- JS/scripts/script.js
+++ JS/scripts/script.js
@@ -1,1 +1,38 @@
-alert("Hello!");
+var x = 10;
+document.write(x);
+var srt1 = "Hello, ",
+    str2 = "World!";
+document.write("<h2>Holla!</h2>");
+var h1 = "<h1>Super!</h1>";
+document.write(h1);
+
+// Всплывающие окна
+// alert("Hi!");
+// confirm("Are you ready?");
+// prompt("what cite you like ?");
+
+// Логирование в консоль
+// console.log("test");
+// console.error("Error!");
+// console.warn("Warning!");
+
+/* Анкета
+var x = confirm("Are you ready?");
+var name = prompt("Please, type your name:");
+var surname = prompt("Please, type your surname:");
+var age = prompt("Please, type your age:");
+var address = prompt("Please, type your address:");
+var gender = prompt("Please, type your gender:");
+document.write("<h1>Анкета:</h1>");
+document.write("<p>Name: <b>" + name + "</b></p>");
+document.write("<p>Surname: <b>" + surname + "</b></p>");
+document.write("<p>Age: <b>" + age + "</b></p>");
+document.write("<p>Address: <b>" + address + "</b></p>");
+document.write("<p>Gender: <b>" + gender + "</b></p>");
+*/
+
+//Калькулятор
+var x= prompt("Please, input x:");
+var y= prompt("Please, input y:");
+x=+x;
+y=+y;
