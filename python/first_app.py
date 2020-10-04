156c08cb78867746466807eef5e7f86326642b5c
Awa1on <strikunm@mail.ru>
2020-10-04 17:42:42 +0500

    next changes

--- python/first_app.py
+++ python/first_app.py
@@ -2,3 +2,22 @@ count = 0
 while count < 5:
     print(count)
     count += 1
+my_list = []
+for i in range(10):
+    my_list.append(i)
+print(my_list)
+another_list = [i for i in range(10)]
+print(another_list)
+fruits = ['apple', 'mango', 'nut', 'banana']
+calories = ['40', '30', '60', '80']
+fruit_cal = {f: c for f, c in zip(fruits, calories)}
+print(fruit_cal)
+even = [i for i in range(20) if i % 2 == 0]
+print(even)
+
+
+def foo(**a):
+    print(a)
+
+
+foo('Hello World!')
